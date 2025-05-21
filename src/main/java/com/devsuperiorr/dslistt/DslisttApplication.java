2987f11e44cc22d50ba9a82ef2810784e2254f11
Valmar Silva <valmar-ssilva@hotmail.com>
2025-05-21 11:52:48 -0300

    Project created listt

---
+++ src/main/java/com/devsuperiorr/dslistt/DslisttApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperiorr.dslistt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslisttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslisttApplication.class, args);
+	}
+
+}
